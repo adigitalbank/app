c4f083324b6ff85fb6f571f84aeda8450f8af98c
Digital Bank <44586554+adigitalbank@users.noreply.github.com>
2021-01-01 17:14:46 +0200

    Update setup.sh

--- adigitalbank-master/setup.sh
+++ adigitalbank-master/setup.sh
@@ -4,7 +4,6 @@
 truffle migrate --reset --network rinkeby
 
 # Verify Contracts on Etherscan
-#truffle run verify DigitalBankToken --network rinkeby --license SPDX-License-Identifier
 truffle run verify ZToken --network rinkeby --license SPDX-License-Identifier
 #truffle run verify yZToken --network rinkeby --license SPDX-License-Identifier
 #truffle run verify SushiBar --network rinkeby --license SPDX-License-Identifier
@@ -13,7 +12,6 @@ truffle run verify MasterChef --network rinkeby --license SPDX-License-Identifie
 # Flatten Contracts
 #./node_modules/.bin/truffle-flattener contracts/SushiToken.sol > flats/SushiToken_flat.sol
 ./node_modules/.bin/truffle-flattener contracts/MasterChef.sol > flats/MasterChef_flat.sol
-#./node_modules/.bin/truffle-flattener contracts/DigitalBankToken.sol > flats/SushiToken_flat.sol
 ./node_modules/.bin/truffle-flattener contracts/ZToken.sol > flats/SushiToken_flat.sol
 ./node_modules/.bin/truffle-flattener contracts/yZToken.sol > flats/SushiToken_flat.sol
 #./node_modules/.bin/truffle-flattener contracts/SushiBar.sol > flats/SushiBar_flat.sol
